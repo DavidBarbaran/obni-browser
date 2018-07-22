04b0c0db7d0f415b23f6a44f0894ffae552ae0b7
JDavidBarbaran <alejandro.david.barbaran@gmail.com>
2018-07-22 05:49:39 -0500

    Start project

---
+++ app/src/main/java/obni/browser/BrowserActivity.kt
@@ -0,0 +1,12 @@
+package obni.browser
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BrowserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browser)
+    }
+}
